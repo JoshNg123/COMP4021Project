98e163b627a454221880c0a843cc32903a0be692
JoshNg123 <jnjw.999@gmail.com>
2024-04-18 23:31:31 +0800

    Set up basic structure of project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # COMP4021Project
+
+## Steps to Run the Program
+
+1. Clone the repo
+2. Run `npm install` to install dependencies
+3. Run `npm start` to run the program
---
+++ server.js
@@ -0,0 +1,12 @@
+const e = require("express");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.json());
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
